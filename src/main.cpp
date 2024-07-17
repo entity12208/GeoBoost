8ab33c8cbaeb5fe0342f826270577c919098276c
entity12208 <116198380+entity12208@users.noreply.github.com>
2024-07-17 02:37:06 -0700

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <Windows.h>
+#include "cheats.h"
+#include "ui.h"
+
+DWORD WINAPI MainThread(LPVOID lpReserved) {
+    if (MH_Initialize() != MH_OK) {
+        return 1;
+    }
+
+    InstallCheats();
+    InitializeImGui();
+
+    while (!GetAsyncKeyState(VK_END)) {
+        if (GetAsyncKeyState(VK_TAB) & 1) {
+            ToggleMenu();
+        }
+
+        RenderImGui();
+    }
+
+    MH_Uninitialize();
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(nullptr, 0, MainThread, hModule, 0, nullptr);
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
